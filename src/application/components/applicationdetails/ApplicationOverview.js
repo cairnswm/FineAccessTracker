fd20b0153e793ace0766f0430025c681e42a1109
William Cairns <cairnswm@gmail.com>
2025-03-27 09:05:31 +0200

    Add a copy to clipboard button for the api key

--- src/application/components/applicationdetails/ApplicationOverview.js
+++ src/application/components/applicationdetails/ApplicationOverview.js
@@ -1,19 +1,35 @@
-import React from "react";
-import { Card } from "react-bootstrap";
+import React, {useState } from "react";
+import { Button, Card } from "react-bootstrap";
+import { Copy } from "react-bootstrap-icons";
 
 const ApplicationOverview = ({ application }) => {
+  const [copied, setCopied] = useState(false);
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
   return (
     <Card className="shadow-sm">
       <Card.Body>
         <Card.Title>Application Overview</Card.Title>
         <Card.Text>
           This dashboard shows analytics for your {application.name}{" "}
-          application. You can view detailed page and item tracking data
-          in the Tracking Data tab.
+          application. You can view detailed page and item tracking data in the
+          Tracking Data tab.
         </Card.Text>
         <div className="d-flex">
           <div className="me-4">
             <strong>API Key:</strong> <code>{application.api_key}</code>
+            <Button
+              variant="light"
+              onClick={() => copyToClipboard(application.api_key)}
+            >
+              <Copy size={16} />
+            </Button>
+            {copied && <span className="text-success ms-2">Copied!</span>}
           </div>
           <div>
             <strong>Created:</strong> {application.created_at}
--- src/application/components/dashboard/TopLocations.js
+++ src/application/components/dashboard/TopLocations.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, ButtonGroup } from "react-bootstrap";
 
 const TopLocations = ({ analytics }) => {
   const [option, setOption] = useState("ip_today");
   const [analyticsToDisplay, setAnalyticsToDisplay] = useState([]);
 
   useEffect(() => {
-    const display = [...analytics].filter(
-      (location) => location[option] > 0
-    );
+    const display = [...analytics].filter((location) => location[option] > 0);
     let sorted = [...display].sort((a, b) => b[option] - a[option]);
     setAnalyticsToDisplay(sorted);
   }, [analytics, option]);
@@ -17,22 +15,53 @@ const TopLocations = ({ analytics }) => {
     <Card className="shadow-sm h-100">
       <Card.Body>
         <Card.Title>Top Locations</Card.Title>
-        <Button variant={option === "ip_last_30_days" ? "primary": "outline-primary"} size="sm" onClick={()=>setOption("ip_last_30_days")}>30 days</Button>
-        <Button variant={option === "ip_last_7_days" ? "primary": "outline-primary"} size="sm" onClick={()=>setOption("ip_last_7_days")}>7 days</Button>
-        <Button variant={option === "ip_yesterday" ? "primary": "outline-primary"} size="sm" onClick={()=>setOption("ip_yesterday")}>Yesterday</Button>
-        <Button variant={option === "ip_today" ? "primary": "outline-primary"} size="sm" onClick={()=>setOption("ip_today")}>Today</Button>
+        <ButtonGroup>
+          <Button
+            variant={
+              option === "ip_last_30_days" ? "primary" : "outline-primary"
+            }
+            size="sm"
+            onClick={() => setOption("ip_last_30_days")}
+          >
+            30 days
+          </Button>
+          <Button
+            variant={
+              option === "ip_last_7_days" ? "primary" : "outline-primary"
+            }
+            size="sm"
+            onClick={() => setOption("ip_last_7_days")}
+          >
+            7 days
+          </Button>
+          <Button
+            variant={option === "ip_yesterday" ? "primary" : "outline-primary"}
+            size="sm"
+            onClick={() => setOption("ip_yesterday")}
+          >
+            Yesterday
+          </Button>
+          <Button
+            variant={option === "ip_today" ? "primary" : "outline-primary"}
+            size="sm"
+            onClick={() => setOption("ip_today")}
+          >
+            Today
+          </Button>
+        </ButtonGroup>
         <ul className="list-group list-group-flush mt-3">
-          {analyticsToDisplay && analyticsToDisplay.slice(0,5).map((location, index) => (
-            <li
-              className="list-group-item d-flex justify-content-between align-items-center"
-              key={index}
-            >
-              {location.country_name}
-              <span className="badge bg-primary rounded-pill">
-                {location[option]} visits
-              </span>
-            </li>
-          ))}
+          {analyticsToDisplay &&
+            analyticsToDisplay.slice(0, 5).map((location, index) => (
+              <li
+                className="list-group-item d-flex justify-content-between align-items-center"
+                key={index}
+              >
+                {location.country_name}
+                <span className="badge bg-primary rounded-pill">
+                  {location[option]} visits
+                </span>
+              </li>
+            ))}
         </ul>
       </Card.Body>
     </Card>
